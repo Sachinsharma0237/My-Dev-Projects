c17f59239c7bfc5133c9a00b95159e6079a1362b
Sachinsharma0237 <75437412+Sachinsharma0237@users.noreply.github.com>
2021-01-28 16:32:30 +0530

    whiteBoard

---
+++ White_Board/web app/public/corner.js
@@ -0,0 +1,37 @@
+const corner = document.querySelector("#corner");
+const cornerOptions = document.querySelector("#corner-options");
+const cornerType = document.querySelector(".corner-type");
+const round = document.querySelector(".round");
+const square = document.querySelector(".square");
+const butt = document.querySelector(".butt");
+
+let lastCornerStyle = "round";
+corner.addEventListener("click", function () {
+    if (corner.classList.contains("active-tool")) {
+        if (cornerOptions.classList.contains("hide")) {
+            cornerOptions.classList.remove("hide");
+        }
+        else {
+            cornerOptions.classList.add("hide");
+        }
+    } else { 
+        ctx.lineCap = lastCornerStyle;
+        if(!pencilOptions.classList.contains("hide") || !eraserOptions.classList.contains("hide")){
+            pencilOptions.classList.add("hide");
+            eraserOptions.classList.add("hide");
+        }
+        pencil.classList.remove("active-tool");
+        eraser.classList.remove("active-tool");
+        corner.classList.add("active-tool");
+    }
+});
+round.addEventListener("click", function(){
+    ctx.lineCap = "round";
+});
+square.addEventListener("click", function(){
+    ctx.lineCap = "square";
+});
+butt.addEventListener("click", function(){
+    ctx.lineCap = "butt";
+});
+
---
+++ White_Board/web app/public/image.js
@@ -0,0 +1,24 @@
+const photo = document.querySelector("#photo");
+const imgUpload = document.querySelector("#photo-upload");
+const download = document.querySelector("#download");
+
+photo.addEventListener("click", function(){
+    imgUpload.click();
+});
+
+download.addEventListener("click", function(){
+    let image = canvas.toDataURL("image/jpeg");
+    let aTag = document.createElement("a");
+    aTag.download = "canvas.jpeg";
+    aTag.href = image;
+    aTag.click();
+});
+  
+imgUpload.addEventListener("change", function(e){
+    let photo = e.target.files[0];
+    let src = URL.createObjectURL(photo);
+    let img = document.createElement("img");
+    img.setAttribute("src", src);
+    let stickyContent = createSticky();
+    stickyContent.append(img);
+});
