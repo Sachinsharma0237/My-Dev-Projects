43859b73ff9fb622f810c50867110dd40bbb1707
Sachinsharma0237 <75437412+Sachinsharma0237@users.noreply.github.com>
2021-02-01 13:17:38 +0530

    change image from popup done

--- Extensions/demoExtension/content.js
+++ Extensions/demoExtension/content.js
@@ -1,11 +1,26 @@
-console.log("hi I'm content file");
-
-let localImages = ["./images/download1.jpg", "./images/download2.jpg","./images/download3.jpg","./images/download4.jpg","./images/download5.jpg","./images/download6.jpg","./images/download7.jpg","./images/download8.jpg","./images/download9.jpg"];
 let images = document.querySelectorAll("img");
 
-for(let i = 0; i < images.length; i++){
+function changeImages(){
+  let localImages = [
+    "./images/download1.jpg",
+    "./images/download2.jpg",
+    "./images/download3.jpg",
+    "./images/download4.jpg",
+    "./images/download5.jpg",
+    "./images/download6.jpg",
+    "./images/download7.jpg",
+    "./images/download8.jpg",
+    "./images/download9.jpg",
+  ];
+  
+  for (let i = 0; i < images.length; i++) {
     let idx = Math.floor(Math.random() * localImages.length);
-    console.log(idx);
     let absolutePath = chrome.extension.getURL(localImages[idx]);
     images[i].src = absolutePath;
+  }
 }
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    changeImages();
+    sendResponse("Image has been changed");
+});
--- Extensions/demoExtension/popup.js
+++ Extensions/demoExtension/popup.js
@@ -1,1 +1,19 @@
-console.log("I'm POPUP!!!!!");
+const changeImageBtn = document.querySelector("#change");
+
+
+
+changeImageBtn.addEventListener("click", function () {
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        let tab = tabs[0];
+        chrome.tabs.sendMessage(tab.id, "hello from popup", function(response) {
+                console.log(response);
+         });
+    });
+});
+
+
+
+
+
+
