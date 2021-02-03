75fd3e700261cb5700ae0652681ec85ec1ff0010
Sachinsharma0237 <75437412+Sachinsharma0237@users.noreply.github.com>
2021-02-03 12:19:12 +0530

    social media blocker complete

--- Extensions/socialMediaBlocker/popup.js
+++ Extensions/socialMediaBlocker/popup.js
@@ -1,34 +1,66 @@
-const input = document.querySelector("#input");
+const input = document.querySelector("#input");     //siteName
 const block = document.querySelector("#block");
-const sites = document.querySelector(".sites");
-const closeButton = document.querySelector("#closeButton");
-let db = [];
+const sites = document.querySelector(".sites");     //blockedList
+const second = document.querySelector(".time");
+
+let localBlockList = [];
+
+chrome.runtime.sendMessage({type : "Get"}, function(sites) {
+        localBlockList = sites;
+        for(let i = 0; i < localBlockList.length; i++){
+            addBlockSitesToUi(localBlockList[i].site, localBlockList[i].time);
+        }
+  });
+
+function addBlockSitesToUi(site, time){
+    let div = document.createElement("div");
+        div.classList.add("alert");
+        div.classList.add("alert-danger");
+        div.innerHTML = site;
+        let closeButton = document.createElement("button");
+        closeButton.classList.add("btn");
+        closeButton.classList.add("btn-outline-danger");
+        closeButton.innerHTML = "X";
+
+        closeButton.addEventListener("click", function(){
+        //closeButton.parentNode.remove();    
+         div.remove();
+         chrome.runtime.sendMessage({type : "Unblock", site}, function(response){
+            console.log(response);
+        });
+     });
+
+        let remain = document.createElement("div");
+        remain.classList.add("remain");
+        remain.innerHTML = time;
+        div.append(remain);
+        div.append(closeButton);
+        sites.append(div);    
+}
 input.addEventListener("keyup", function(e){
     if( e.keyCode == 13 ){
         block.click();
     }
-})
-
+});
+second.addEventListener("keyup", function(e){
+    if( e.keyCode == 13 ){
+        block.click();
+    }
+});
 
 block.addEventListener("click", function(){
-    let text = input.value;
-    if(text){
-        db.push(text);
-        let site = document.createElement("div");
-        site.classList.add("alert");
-        site.classList.add("alert-danger");
-        site.innerHTML = text;
-        let closeButton = document.createElement("div");
-        closeButton.classList.add("btn");
-        closeButton.classList.add("btn-outline-danger");
-        closeButton.innerHTML = "X";
-        site.append(closeButton);
-        sites.append(site);
+    let site = input.value;
+    let time = second.value;
+    if(site){
+        addBlockSitesToUi(site, time);
         input.value = "";
-        closeButton.addEventListener("click", function(){
-            closeButton.parentNode.remove();    // site.remove();
-            db.pop();
-        })
+        second.value = "";
     }
+    //yha pr popup ne bola ki iss site ko block kr de itne time k liye background ko....q ki background k pass authority hai block krne ki
+    chrome.runtime.sendMessage({type:"Add", site, time}, function(response) {
+        console.log(response);
+      });
+
+
 
 });
