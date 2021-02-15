71c18cebb9d070b4d08180e8cffad1142a225b10
Sachinsharma0237 <75437412+Sachinsharma0237@users.noreply.github.com>
2021-02-15 13:13:21 +0530

    multer added

--- InstaClone/backend/app.js
+++ InstaClone/backend/app.js
@@ -3,16 +3,13 @@ const userRouter = require("./router/userRouter");
 const app = express();
 
 
+app.use( express.static("public") );
 
 app.use( express.json() )
 
 //for all user related functions navigate to userRouter
 app.use("/api/user", userRouter);
 
-
-// app.get("/api/user/:id", function(req, res){
-//     console.log(req.params.id);
-// })
 
 let port = process.env.PORT || 3000;
 app.listen(port, function(){
--- InstaClone/backend/router/userRouter.js
+++ InstaClone/backend/router/userRouter.js
@@ -1,10 +1,36 @@
+const multer = require("multer");
+const path = require("path");
 const { createUser, getAllUsers, getUserById, updateUserById, deleteUserById } = require("../controller/userController");
 const userRouter = require("express").Router();
 
 
-userRouter.route("").get(getAllUsers).post(createUser);
+const storage = multer.diskStorage({
+    destination : function (req, file, cb) {
+        cb(null, 'public/images/users');
+    },
+    filename : function (req,  file, cb) {
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname));
+    }
+});
+const fileFilter = function (req, file, cb) {
+    if( file.mimetype == 'image/jpg' || file.mimetype == 'image/jpeg' || file.mimetype == 'image/png'){
+        cb(null, true);
+    }else{
+        cb(null, false);
+    }
+}
 
-userRouter.route("/:id").get(getUserById).patch(updateUserById).delete(deleteUserById);
+const upload = multer({storage:storage, fileFilter:fileFilter});
+
+userRouter.route("").get(getAllUsers).post(upload.single('user'), createUser );
+
+
+
+userRouter.route("/:id")
+.get(getUserById)
+.patch(upload.single('user') ,updateUserById)
+.delete(deleteUserById);
 
 
 
